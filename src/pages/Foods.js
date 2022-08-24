e1acb9b4eb883855002634cb002c1428f2ad4f77
Vitormonteiro28 <vitormonteirotomaz7@gmail.com>
2022-08-24 15:33:59 -0300

    testando

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,32 +1,22 @@
 import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
-import Footer from '../components/Footer';
+import { useHistory } from 'react-router-dom';
 import AppContext from '../AppContext/AppContext';
 import Header from '../components/Header';
 
 export default function Foods() {
   const { recipes } = useContext(AppContext);
-  const max = 12;
-
+  const history = useHistory();
   return (
     <div>
       <Header title="Foods" search />
       {
-        recipes.length === 1 && <Redirect to={ `/foods/${recipes[0].idMeal}` } />
+        recipes.length === 1 && history.push(`/foods/${recipes[0].idMeal}`)
       }
       {
-        recipes.length > 1 && recipes.slice(0, max).map((item, i) => (
-          <div key={ item.strMeal } data-testid={ `${i}-recipe-card` }>
-            <img
-              src={ item.strMealThumb }
-              alt={ item.strMeal }
-              data-testid={ `${i}-card-img` }
-            />
-            <h3 data-testid={ `${i}-card-name` }>{item.strMeal}</h3>
-          </div>
+        recipes.length > 1 && recipes.map((item, i) => (
+          <div key={ i }>{item.strMeal}</div>
         ))
       }
-      <Footer />
     </div>
   );
 }
